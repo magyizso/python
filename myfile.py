58a0e62563e6e48b53d8ae27bd157b1eb36296b1
magyizso <magyizso@gmail.com>
2022-07-04 12:17:34 +0200

    Python first steps

---
+++ myfile.py
@@ -0,0 +1,66 @@
+"""
+This is a 
+comment in
+multiline strings.
+
+if 5>2:
+ print("Five is greater than two")
+ # print("Five is greater than two")
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(type(x))
+print(type (y))
+print(type(z))
+
+a=3
+A='Sally'
+
+print(a)
+print(A)
+
+fruits=["apple","banana","cherry"]
+x, y, z= fruits
+print(x)
+print(y)
+print(z)
+
+x="Python "
+y="is "
+z="awesome!"
+print(x+y+z)
+
+print(a,A)
+"""
+"""
+x="awsome!"
+
+def myfunc():
+    global x
+    x="fantastic"
+    print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+y= 1j
+print(type(y))
+
+import random
+
+print(random.randrange(1,100))
+
+
+a="Hello world!"
+print(a[1])
+
+for x in "banana":
+    print(x)
+print(len(a))
+"""
+txt= "The best things in life is free!"
+print("expensive" not in txt)
+#if "expensive" not in txt:
+ #   print("No, 'expensive' is not present.")
