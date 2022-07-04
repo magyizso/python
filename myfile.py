0f9e77fc5567fd62f84b8a6368371f6301b66ed3
magyizso <magyizso@gmail.com>
2022-07-04 22:00:19 +0200

    Python afternoon

--- myfile.py
+++ myfile.py
@@ -56,11 +56,55 @@ print(random.randrange(1,100))
 a="Hello world!"
 print(a[1])
 
-for x in "banana":
-    print(x)
+#for x in "banana":
+ #   print(x)
 print(len(a))
-"""
+
 txt= "The best things in life is free!"
-print("expensive" not in txt)
-#if "expensive" not in txt:
- #   print("No, 'expensive' is not present.")
+#print("expensive" not in txt)
+if "expensive" not in txt:
+   print("No, 'expensive' is not present.")
+
+b="Hello world!"
+print(b[-5:-2])
+
+a=" Hello, world! "
+b=a.upper()
+c=b.lower()
+print(a)
+print(b)
+print(c)
+print(a.strip())
+print(a.replace("H","J"))
+print(a.split(","))
+
+a="Hello"
+b="World!"
+c=a+" "+b
+print(c)
+
+age=36
+txt="My name is John, and I am {}"
+print(txt.format(age))
+
+quantity= 3
+itemno= 567
+price= 49.95
+myorder= "I want to pay {2} dollars for {0} pieces of {1} items"
+print(myorder.format(quantity, itemno, price))
+"""
+a= 200
+b= 33
+
+if b>a :
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+
+
+
+
+
+
+
