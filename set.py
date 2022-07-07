70a3bfce78576d01ea17e2ca194738db1cb420e7
magyizso <magyizso@gmail.com>
2022-07-07 16:22:24 +0200

    set

---
+++ set.py
@@ -0,0 +1,12 @@
+myset = set(("apple", "banana", "cherry"))
+print(type(myset))
+#myset.add("orange")
+tropical = {"pineapple", "mango", "papaya"}
+tropical2 = ["kiwi", "orange"]
+myset.update(tropical2)
+z = myset.pop()
+print(z)
+print(myset)
+#for x in myset:
+ #   print(x)
+#print("banna" in myset)
