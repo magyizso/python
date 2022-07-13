757065321ae002f12d1c7b196d6e60d1d50df21e
magyizso <magyizso@gmail.com>
2022-07-13 15:56:19 +0200

    matplotlib

--- matplotlib/plotting3.py
+++ matplotlib/plotting3.py
@@ -8,7 +8,10 @@ import numpy as np
 ypoints = np.array([3, 8, 1, 10, 5, 7])
 
 #plt.plot(ypoints, 'x-.r')
-plt.plot(ypoints, marker = 'o', ms = 20, mfc = '#4caf50', mec = 'r')
+#plt.plot(ypoints, marker = 'o', ms = 20, mfc = '#4caf50', mec = 'r')
+#plt.plot(ypoints, linestyle= 'dashed')
+#plt.plot(ypoints, ls= '-.', color= 'r')
+plt.plot(ypoints, c= 'hotpink', linewidth= 20.5)
 plt.show()
 
 plt.savefig(sys.stdout.buffer)
